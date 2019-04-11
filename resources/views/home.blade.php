14ec64ca086e20bce7e64e098f29fc5e55f1eabe
杨立培 <747136311@qq.com>
2019-04-11 13:21:46 +0000

    基础页面

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Weibo App</title>
-</head>
-<body>
-<h1>主页</h1>
-</body>
-</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,3 @@
 Route::get('/','StaticPagesController@home');
 Route::get('/help','StaticPagesController@help');
 Route::get('/about','StaticPagesController@about');
-Route::get('/', function () {
-    return view('welcome');
-});
