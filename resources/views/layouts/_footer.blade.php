12d03d4c660b4ef7ae8edc03734aa90653caf2c5
杨立培 <747136311@qq.com>
2019-04-12 13:42:48 +0000

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="footer">
-    <img class="brand" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="">
+    <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="">
     <a href="https://learnku.com/laravel/courses" target=_blank>
         刻意练习,每日精进.
     </a>
 
     <div class="float-right">
-        <a href="/about">关于</a>
+        <a href="{{ route('about') }}">关于</a>
     </div>
 </footer>
