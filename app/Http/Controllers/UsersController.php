cc330e0e247acdfea296e0672020bf145489bd93
杨立培 <747136311@qq.com>
2019-04-13 07:34:19 +0000

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,18 @@ class UsersController extends Controller
     public function create(){
         return view('users.create');
     }
+
     public function show(User $user){
         return view('users.show',compact('user'));
     }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'name'=>'required|max:50',
+            'email'=>'required|email|unique:users|max:255',
+            'password'=>'required|confirmed|min:6'
+        ]);
+        return;
+    }
+
 }
