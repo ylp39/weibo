5157daf3e1ac4d086144fa2f23695ccd1311f6a6
杨立培 <747136311@qq.com>
2019-04-14 14:57:30 +0000

    xiugai USER

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 class SessionsController extends Controller
 {
     public function create(){
