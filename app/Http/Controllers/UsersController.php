eaa3afa28c70d4e9a3073e9878873a66035a6bd7
杨立培 <747136311@qq.com>
2019-04-16 08:13:49 +0000

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,6 +12,10 @@ class UsersController extends Controller
         $this->middleware('auth',[
             'except' => ['show','create','store']
         ]);
+
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
     }
 
     //
@@ -41,10 +45,12 @@ class UsersController extends Controller
     }
 
     public function edit(User $user){
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     public function update(User $user,Request $request){
+        $this->authorize('update',$user);
         $this->validate($request,[
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6',
