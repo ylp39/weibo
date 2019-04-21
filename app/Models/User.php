65ffd9132b6bac097fb2748abd4ec17becceb3f1
杨立培 <747136311@qq.com>
2019-04-21 07:40:07 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,6 +57,39 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+
+    public function followers()
+    {
+     return $this->belongsToMany(User::class,
+         'followers','user_id','follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,
+            'followers','follower_id','user_id');
+    }
+    
     public function gravatar($size = '100'){
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
